9ff982c4968da270a4403054d9428662c3bead4b
Alfredo <pepe.skarcho17@gmail.com>
2021-07-08 23:45:44 -0500

    Se arreglo la falla y se añadieron nuevas funciones, como datos adicionales al heroe y boton de regresar

--- src/components/heroes/HeroesScreen.js
+++ src/components/heroes/HeroesScreen.js
@@ -1,18 +1,70 @@
-import {useEffect} from 'react';
-import { useParams } from 'react-router-dom';
-//import { getHeroesById } from '../../selectors/getHeroesById';
+import React from 'react';
+import { useParams, Redirect } from 'react-router-dom';
+import { getHeroesById } from '../../selectors/getHeroesById';
 
-export const HeroesScreen = () => {
+export const HeroesScreen = ({ history }) => {
     
-    const params = useParams();
-    useEffect(() => {
-        console.log(params);
-      });
+    const { heroeId } = useParams();
+
+    const hero = getHeroesById( heroeId );
+
+    if (!hero){
+        return <Redirect to = "/"/>
+    }
+
+    const handleReturn = () => {
+
+        if(history.length <=2){
+            history.push('/');
+        }else{
+            history.goBack();
+        }
+    }
+    
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero
     
 
     return (
-        <div>
-            <h1>HEROES Screen</h1>
+        <div className = "row mt-5">
+            <div className = "col-4">
+                <img
+                    src = { `../assets/heroes/${ heroeId }.jpg` }
+                    alt = { superhero }
+                    className = "img-thumbnail"
+                />
+            </div>
+
+            <div className = "col-8">
+                <h3><i>{superhero}</i></h3>
+                <ul className = "list-group list-group-flush">
+                    <li className = "list-group-item"><b>Alter ego: </b> {alter_ego} </li>
+                    <li className = "list-group-item"><b>Publisher: </b> {publisher} </li>
+                    <li className = "list-group-item"><b>First Appearance: </b> {first_appearance} </li>
+                </ul>
+                <hr/>
+                <div className = "col-2">
+                <ul className = "list-group list-group-flush">
+                    <li className = "list-group-item"><b>Characters: </b> {characters} </li>
+                </ul>    
+                </div>
+
+                <div className = "col-2">
+                    <button 
+                        className = "btn btn-primary mt-2"
+                        onClick = {handleReturn}
+                    >
+                        Regresar
+                    </button>
+                </div>
+
+            </div>    
+
         </div>
     )
 }
